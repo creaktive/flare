44f412b6109dd561655ac3e2ab619e3640cbf42c
Stanislaw Pusep <creaktive@gmail.com>
2014-11-15 18:07:22 +0100

    Implemented a simplified GFSK demodulator

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ```bash
 make
-rtl_fm -f 868.3m -s 1.6m -g 49.6 -p 49 - | ./nrf905_demod | ./flarm_decode 43.21 5.43
+rtl_sdr -f 868.3m -s 1.6m -g 49.6 -p 49 - | ./nrf905_demod | ./flarm_decode 43.21 5.43 12
 ```
 
 # References
@@ -11,3 +11,6 @@ rtl_fm -f 868.3m -s 1.6m -g 49.6 -p 49 - | ./nrf905_demod | ./flarm_decode 43.21
  - [G.9959(ZWave) demodulator](https://github.com/andersesbensen/rtl-zwave)
  - [Sniffing and decoding NRF24L01+ and Bluetooth LE packets for under $30](http://blog.cyberexplorer.me/2014/01/sniffing-and-decoding-nrf24l01-and.html)
  - [Using a RTLSDR dongle to validate NRF905 configuration](http://www.embeddedrelated.com/showarticle/548.php)
+ - [Implementation of Digital Signal Processing: Some Background on GFSK Modulation](http://wwwhome.ewi.utwente.nl/~gerezsh/sendfile/sendfile.php/gfsk-intro.pdf?sendfile=gfsk-intro.pdf)
+ - [An Arctangent Type Wideband PM/FM Demodulator with Improved Performances](http://ketabkhanemelli.com/Scientific/IEEE/iel2/565/3783/00140755.pdf)
+ - [nRF905 Single chip 433/868/915MHz Transceiver Product Specification](http://www.nordicsemi.com/eng/content/download/2452/29528/file/Product_Specification_nRF905_v1.5.pdf)
--- nrf905_demod.c
+++ nrf905_demod.c
@@ -1,3 +1,4 @@
+#include <float.h>
 #include <limits.h>
 #include <math.h>
 #include <stdint.h>
@@ -17,17 +18,68 @@
  * nRF905 preamble is Manchester-encoded 0x3F6 (0xAAA66):
  * '10101010101001100110'
  */
-static const uint8_t hi_map[PREAMBLE_BITS] = { 1, 3, 5, 7, 9, 11, 12, 15, 16, 19 };
-static const uint8_t lo_map[PREAMBLE_BITS] = { 0, 2, 4, 6, 8, 10, 13, 14, 17, 18 };
+const uint8_t hi_map[PREAMBLE_BITS] = { 1, 3, 5, 7, 9, 11, 12, 15, 16, 19 };
+const uint8_t lo_map[PREAMBLE_BITS] = { 0, 2, 4, 6, 8, 10, 13, 14, 17, 18 };
+
+/* Digital filter designed by mkfilter/mkshape/gencode   A.J. Fisher
+ *    Command line: /www/usr/fisher/helpers/mkfilter -Bu -Lp -o 6 -a 1.1718750000e-01 0.0000000000e+00 -l */
+__attribute__((always_inline)) double lowpass_i(double v) {
+    static double xv[7], yv[7];
+
+    xv[0] = xv[1]; xv[1] = xv[2]; xv[2] = xv[3]; xv[3] = xv[4]; xv[4] = xv[5]; xv[5] = xv[6];
+    xv[6] = v / 1.313502153e+03;
+    yv[0] = yv[1]; yv[1] = yv[2]; yv[2] = yv[3]; yv[3] = yv[4]; yv[4] = yv[5]; yv[5] = yv[6];
+    yv[6] = (xv[0] + xv[6]) + 6 * (xv[1] + xv[5]) + 15 * (xv[2] + xv[4])
+        + 20 * xv[3]
+        + ( -0.0534038233 * yv[0]) + (  0.4717508964 * yv[1])
+        + ( -1.7864915891 * yv[2]) + (  3.7327266053 * yv[3])
+        + ( -4.5793923315 * yv[4]) + (  3.1660855409 * yv[5]);
+
+    return yv[6];
+}
+
+__attribute__((always_inline)) double lowpass_q(double v) {
+    static double xv[7], yv[7];
+
+    xv[0] = xv[1]; xv[1] = xv[2]; xv[2] = xv[3]; xv[3] = xv[4]; xv[4] = xv[5]; xv[5] = xv[6];
+    xv[6] = v / 1.313502153e+03;
+    yv[0] = yv[1]; yv[1] = yv[2]; yv[2] = yv[3]; yv[3] = yv[4]; yv[4] = yv[5]; yv[5] = yv[6];
+    yv[6] = (xv[0] + xv[6]) + 6 * (xv[1] + xv[5]) + 15 * (xv[2] + xv[4])
+        + 20 * xv[3]
+        + ( -0.0534038233 * yv[0]) + (  0.4717508964 * yv[1])
+        + ( -1.7864915891 * yv[2]) + (  3.7327266053 * yv[3])
+        + ( -4.5793923315 * yv[4]) + (  3.1660855409 * yv[5]);
+
+    return yv[6];
+}
+
+/* Digital filter designed by mkfilter/mkshape/gencode   A.J. Fisher
+ *    Command line: /www/usr/fisher/helpers/mkfilter -Bu -Lp -o 6 -a 6.2500000000e-02 0.0000000000e+00 -l */
+__attribute__((always_inline)) double lowpass_d(double v) {
+    static double xv[7], yv[7];
+
+    xv[0] = xv[1]; xv[1] = xv[2]; xv[2] = xv[3]; xv[3] = xv[4]; xv[4] = xv[5]; xv[5] = xv[6];
+    xv[6] = v / 3.469103409e+04;
+    yv[0] = yv[1]; yv[1] = yv[2]; yv[2] = yv[3]; yv[3] = yv[4]; yv[4] = yv[5]; yv[5] = yv[6];
+    yv[6] = (xv[0] + xv[6]) + 6 * (xv[1] + xv[5]) + 15 * (xv[2] + xv[4])
+        + 20 * xv[3]
+        + ( -0.2165828556 * yv[0]) + (  1.6277147849 * yv[1])
+        + ( -5.1476426814 * yv[2]) + (  8.7791079706 * yv[3])
+        + ( -8.5290050840 * yv[4]) + (  4.4845630084 * yv[5]);
+
+    return yv[6];
+}
 
 void process_stream(FILE *stream) {
-    int16_t readbuf[BUFFER_SIZE];
+    uint8_t readbuf[BUFFER_SIZE << 1];
     uint16_t c;
     size_t len;
 
-    static double xv[4], yv[4];
+    static double i1, q1, i2, q2;
+    double m, d;
 
-    static int16_t buffer[BUFFER_SIZE];
+    static int16_t buffer[BUFFER_SIZE]; // demodulated signal
+    static int16_t m_buff[BUFFER_SIZE]; // magnitudes only; for RSSI estimation
     static uint16_t buffer_end = 0;
     static uint16_t buffer_skip = PREAMBLE_SIZE + 2 * SAMPLE_RATE * MAX_MSG_SIZE * CHAR_BIT;
     static int32_t sum = 0;
@@ -47,21 +99,19 @@ void process_stream(FILE *stream) {
     char out[128], *p;
 
     while (!feof(stream)) {
-        len = fread(readbuf, sizeof(int16_t), BUFFER_SIZE, stream);
+        len = fread(readbuf, sizeof(uint16_t), BUFFER_SIZE, stream);
 
-        for (c = 0; c < len; c++) {
-            /* Digital filter designed by mkfilter/mkshape/gencode   A.J. Fisher
-               Command line: /www/usr/fisher/helpers/mkfilter -Bu -Lp -o 3 -a 6.2500000000e-02 0.0000000000e+00 -l */
+        for (c = 0; c < len << 1; c += 2) {
+            i1 = lowpass_i(readbuf[c + 0] - 127);
+            q1 = lowpass_q(readbuf[c + 1] - 127);
+            m = i1 * i1 + q1 * q1;
+            d = (i1 * (q1 - q2) - q1 * (i1 - i2)) / (m + DBL_MIN);
+            i2 = i1;
+            q2 = q1;
 
-            xv[0] = xv[1]; xv[1] = xv[2]; xv[2] = xv[3];
-            xv[3] = readbuf[c] / 1.886646578e+02;
-            yv[0] = yv[1]; yv[1] = yv[2]; yv[2] = yv[3];
-            yv[3] = (xv[0] + xv[3]) + 3 * (xv[1] + xv[2])
-                + (  0.4535459334 * yv[0])
-                + ( -1.7151178300 * yv[1])
-                + (  2.2191686183 * yv[2]);
-
-            buffer[buffer_end++] = yv[2];
+            m_buff[buffer_end] = m;
+            buffer[buffer_end] = lowpass_d(d * SHRT_MAX);
+            buffer_end++;
             buffer_end &= BUFFER_SIZE - 1;
 
             sum -= BUF(BUFFER_SIZE);
@@ -105,13 +155,13 @@ void process_stream(FILE *stream) {
                                 timestamp -= (BUFFER_SIZE / SAMPLE_RATE) * 2e-5;
 
                                 for (k = 0, rms = 0; k < i; k++)
-                                    rms += BUF(k) * BUF(k);
+                                    rms += m_buff[k] * m_buff[k];
                                 rms /= i;
 
                                 snprintf(out + msg_len * 2, sizeof(out) + msg_len * 2,
                                         "\t%.06f\t%.01f",
                                         timestamp,
-                                        20.0 * log10(sqrt(rms) / SHRT_MAX)
+                                        20.0 * log10(SHRT_MAX / sqrt(rms))
                                 );
 
                                 puts(out);
